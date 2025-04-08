04cd5cfeebb24aac60b69d600000818e4d2f1fc2
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-04-08 12:17:25 -0400

    adding factory | cls Methods

--- classes.py
+++ classes.py
@@ -1,20 +1,31 @@
 class Mypuppy:
-  def __init__(self,dogName):
+  def __init__(self, dogName):
     self.name = dogName
 
+  @classmethod
+  def bark(cls):
+    print("Guau!")
+
   def bark(self):
-      print("woooh!")
+    print("woooh!")
+
+  @classmethod
+  def factory(cls):
+    return cls("Happy Rex")
+
+
 
 my_dog = Mypuppy("Casio")
 
 print(type(my_dog))
-
 my_dog.bark()
-print(isinstance(my_dog,Mypuppy))
+print(isinstance(my_dog, Mypuppy))
 
 my_dog = Mypuppy("Rocky")
-my_dog2 = Mypuppy("lola")
+my_dog2 = Mypuppy("Lola")
 
 print(my_dog.name)
 print(my_dog2.name)
 
+my_dog3 = Mypuppy.factory()
+print(my_dog3.name)
